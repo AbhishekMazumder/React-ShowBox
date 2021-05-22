7913d0d2a3c986dfe360e727ab4d044852627143
abhishek <abhishekmazumder69@gmail.com>
2021-05-22 10:29:04 +0530

    Search functionality implemented and with this version 1.0 completed.

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,66 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import PageTitle from '../../components/PageTitle';
 import {
+	Button,
 	createMuiTheme,
+	Tab,
+	Tabs,
+	TextField,
 	ThemeProvider,
-	makeStyles,
-} from '@material-ui/core/styles';
+	makeStyles
+} from '@material-ui/core';
+import './Search.css';
 import SearchIcon from '@material-ui/icons/Search';
-import { Button } from '@material-ui/core';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import CustomPagination from '../../components/Pagination/CustomPagination';
 import SingleContent from '../../components/SingleContent/SingleContent';
 
-const darkTheme = createMuiTheme({
-	palette: {
-		type: 'dark',
-		primary: {
-			main: '#fff',
-		},
-	},
-});
-
-const useStyles = makeStyles({
-	search_Wrapper: {
-		display: 'flex',
-		alignItems: 'center',
-		border: '2px solid grey',
-		borderRadius: '6px',
-		marginTop: '15px',
-	},
-	search_Input: {
-		flex: 1,
-		// padding: '10px',
-		marginLeft: '15px',
-		backgroundColor: 'transparent',
-		border: 'none',
-		color: 'white',
-		fontSize: '20px',
-		outline: 'none',
-	},
-	tab_wrapper: {
-		width: '100%',
-		flexGrow: 1,
-		marginTop: '10px',
-	},
-});
-
 const Search = () => {
-	const classes = useStyles();
 	const [type, setType] = useState(0);
+	const [searchText, setSearchText] = useState('');
 	const [page, setPage] = useState(1);
-	const [searchTerm, setSearchTerm] = useState('');
 	const [content, setContent] = useState([]);
 	const [numOfPages, setNumOfPages] = useState();
+
+	const darkTheme = createMuiTheme({
+		palette: {
+			type: 'dark',
+			primary: {
+				main: '#fff',
+			},
+		},
+	});
 
 	const fetchSearch = async () => {
 		try {
 			const { data } = await axios.get(
 				`https://api.themoviedb.org/3/search/${type ? 'tv' : 'movie'}?api_key=${
 					process.env.REACT_APP_API_KEY
-				}&language=en-US&query=${searchTerm}&page=${page}&include_adult=false`
+				}&language=en-US&query=${searchText}&page=${page}&include_adult=false`
 			);
 			setContent(data.results);
 			setNumOfPages(data.total_pages);
@@ -78,42 +53,63 @@ const Search = () => {
 
 	return (
 		<div>
-			<PageTitle title="Search movies and series" />
 			<ThemeProvider theme={darkTheme}>
-				<div className={classes.search_Wrapper}>
-					<input
-						type="text"
-						value={searchTerm}
-						placeholder="Type here to search"
-						className={classes.search_Input}
-						onChange={e => setSearchTerm(e.target.value)}
+				<div className="search">
+					<TextField
+						style={{ flex: 1 }}
+						className="searchBox"
+						label="Search"
+						variant="filled"
+						onChange={e => setSearchText(e.target.value)}
 					/>
 					<Button
-						style={{ borderRadius: 0, paddingLeft: '10px' }}
 						onClick={fetchSearch}
+						variant="contained"
+						style={{ marginLeft: 10 }}
 					>
 						<SearchIcon fontSize="large" />
 					</Button>
 				</div>
-				<div className={classes.tab_wrapper}>
-					<Tabs
-						value={type}
-						indicatorColor="primary"
-						textColor="primary"
-						variant="fullWidth"
-						onChange={(event, newValue) => {
-							setType(newValue);
-							setPage(1);
-						}}
-						style={{ padding: '5px 0px' }}
-					>
-						<Tab label="Search Movies" style={{ width: '50%' }} />
-						<Tab label="Search Series" style={{ width: '50%' }} />
-					</Tabs>
-				</div>
+				<Tabs
+					value={type}
+					indicatorColor="primary"
+					textColor="primary"
+					variant="fullWidth"
+					onChange={(event, newValue) => {
+						setType(newValue);
+						setPage(1);
+					}}
+					style={{ paddingBottom: 5 }}
+					aria-label="disabled tabs example"
+				>
+					<Tab style={{ width: '50%' }} label="Search Movies" />
+					<Tab style={{ width: '50%' }} label="Search TV Series" />
+				</Tabs>
 			</ThemeProvider>
+			{searchText &&
+				!content &&
+				(type ? <h2>No Series Found</h2> : <h2>No Movies Found</h2>)}
+			<div className="content-wrapper">
+				{content &&
+					content.map(c => (
+						<SingleContent
+							key={c.id}
+							id={c.id}
+							poster={c.poster_path}
+							title={c.title || c.name}
+							date={c.first_air_date || c.release_date}
+							media_type={type ? 'tv' : 'movie'}
+							ratings={c.vote_average}
+						/>
+					))}
+			</div>
+			{numOfPages > 1 && (
+				<CustomPagination setPage={setPage} numOfPages={numOfPages} />
+			)}
 		</div>
 	);
 };
+
+
 
 export default Search;
